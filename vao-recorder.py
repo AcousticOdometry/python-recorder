fbbd9b8567e95dab5522ff9bc0699a11214454c1
esdandreu <esdandreu@gmail.com>
2022-02-08 19:31:04 +0100

    failed to read own tests with python, but recording works

--- vao-recorder.py
+++ vao-recorder.py
@@ -3,9 +3,8 @@ import soundfile as sf
 import typer
 import yaml
 
+from typing import Optional, List
 from PyInquirer import prompt
-from functools import partial
-from typing import Optional
 from datetime import datetime
 from pathlib import Path
 
@@ -18,29 +17,6 @@ MICROPHONES = {
     i: d
     for i, d in enumerate(sd.query_devices()) if d['max_input_channels'] > 0
     }
-
-
-def record_audio(
-        device_id: int,
-        output_path: Path,
-        samplerate: int = None,
-        channels: int = None,
-        **stream_kwargs
-    ) -> sd.InputStream:
-    with sf.SoundFile(
-        output_path, 'x', samplerate=samplerate, channels=channels
-        ) as f:
-        return sd.InputStream(
-            device=device_id,
-            samplerate=samplerate,
-            channels=channels,
-            callback=lambda data, frames, time, status: f.write(data),
-            **stream_kwargs
-            )
-
-
-def record_video(device_id, output_folder):
-    pass
 
 
 @app.command()
@@ -56,8 +32,6 @@ def config(
             help="Path to where the configuration yaml file should be written."
             )
     ) -> dict:
-    # yaml microphones should match the call signature of sounddevice.InputStream
-    # https://python-sounddevice.readthedocs.io/en/0.4.4/api/streams.html#sounddevice.InputStream
     config = {'microphones': {}, 'cameras': {}}
     while typer.confirm(
         f"Do you want to add {'another' if config['microphones'] else 'a'} "
@@ -99,29 +73,38 @@ def get_config(path=DEFAULT_CONFIG_PATH) -> dict:
     return _config
 
 
-@app.command(help='Record visual and acoustic data.')
-def record(
-        config_path: Optional[Path] = typer.Option(
-            DEFAULT_CONFIG_PATH,
-            help="""
-            Path to a `yaml` config file. Run `config` command to generate one.
-            """
-            ),
-        output_folder: Optional[Path] = typer.Option(
-            DEFAULT_OUTPUT_FOLDER,
-            help="""
-            Path to a folder where the recorded data and the recording
-            configuration will be stored."""
-            )
-    ) -> Path:
-    config = get_config(config_path)
-    output_folder.mkdir(exist_ok=True, parents=True)
+def get_audio_stream(
+    device_id: int,  # Device identifier
+    output_path: Path,  # Path to where the audio file should be written
+    samplerate: int = None,  # Sample rate
+    channels: int = None,  # Number of channels
+    **
+    stream_kwargs,  # Additional keyword arguments for sounddevice.InputStream
+    # https://python-sounddevice.readthedocs.io/en/0.4.4/api/streams.html#sounddevice.InputStream
+    ) -> sd.InputStream:
+    f = sf.SoundFile(
+        output_path, 'x', samplerate=int(samplerate), channels=int(channels)
+        )
+    return sd.InputStream(
+        device=device_id,
+        samplerate=samplerate,
+        channels=channels,
+        callback=lambda data, frames, time, status: f.write(data),
+        finished_callback=f.close,
+        **stream_kwargs
+        )
+
+
+def get_audio_streams(
+        config: dict,  # Configuration dictionary
+        output_folder: Path,  # Folder where the audio files should be written
+    ) -> List[sd.InputStream]:
     streams = []
     for i, (_id, mic) in enumerate(config.get('microphones', {}).items()):
         name = f"{_id} {mic.pop('name', '')}"
         try:
             streams.append(
-                record_audio(
+                get_audio_stream(
                     device_id=_id,
                     output_path=output_folder / f'audio{i}.wav',
                     **mic
@@ -129,26 +112,98 @@ def record(
                 )
         except TypeError as e:
             typer.secho(
-                f'Failed to open microphone `{name}`, review the '
-                f'configuration file {config_path}\nError: {e}',
+                f'Failed to open microphone `{name}`, review its '
+                f'configuration {mic}\nError: {e}',
                 fg='red'
                 )
-            raise typer.Exit(code=1)
-        # TODO write metadata in audio{i}.txt
+            raise e
+        # Write configuration in a yaml file
+        with open(output_folder / f'audio{i}.yaml', 'w') as f:
+            f.write(yaml.dump(mic))
+    return streams
+
+
+def record_video(device_id, output_folder):
+    pass
+
+
+@app.command(help='Record visual and acoustic data.')
+def record(
+    config_path: Optional[Path] = typer.Option(
+        DEFAULT_CONFIG_PATH,
+        help="""
+        Path to a `yaml` config file. Run `config` command to generate one."""
+        ),
+    output_folder: Optional[Path] = typer.Option(
+        DEFAULT_OUTPUT_FOLDER,
+        help="""
+        Path to a folder where the recorded data and the recording
+        configuration will be stored."""
+        ),
+    ) -> Path:
+    config = get_config(config_path)
+    output_folder.mkdir(exist_ok=True, parents=True)
+
+    # Setup audio recording
+    audio_streams = get_audio_streams(config, output_folder)
+
+    # Setup video recording
     for camera in config.get('cameras', []):
+        # TODO read video cameras
         typer.echo(camera)
+
+    # Start the recording
+    for stream in audio_streams:
+        stream.start()
+    # Wait for user input
+    input('Press `Enter` to stop recording.')
+    # Cleanup the recording
+    for stream in audio_streams:
+        stream.stop()
+        stream.close()
+    return output_folder
+
+
+@app.command(
+    help="""
+    Record 5 seconds from the configured microphones and play back the audio of
+    each recording."""
+    )
+def test_audio(
+    config_path: Optional[Path] = typer.Option(
+        DEFAULT_CONFIG_PATH,
+        help="""
+        Path to a `yaml` config file. Run `config` command to generate one."""
+        ),
+    output_folder: Optional[Path] = typer.Option(
+        DEFAULT_OUTPUT_FOLDER,
+        help="""
+        Path to a folder where the recorded data and the recording
+        configuration will be stored."""
+        ),
+    ) -> Path:
+    config = get_config(config_path)
+    output_folder.mkdir(exist_ok=True, parents=True)
+    print(sf._libname)
+    print(sf.available_formats())
+    # Record 5 seconds of audio
+    audio_streams = get_audio_streams(config, output_folder)
+    for stream in audio_streams:
+        stream.start()
+    with typer.progressbar(range(50), label='Recording...') as progress:
+        for _ in progress:
+            sd.sleep(100)  # milliseconds
+    for stream in audio_streams:
+        stream.stop()
+        stream.close()
+    sd.sleep(1000)
+    # Play audio
+    for _file in output_folder.glob('audio*.wav'):
+        typer.echo(f'Playing {_file}')
+        with open(_file, 'r') as f:
+            data, fs = sf.read(f, always_2d=True)
+            sd.play(data, samplerate=fs)
 
 
 if __name__ == '__main__':
     app()
-
-    # fs = 48000  # Sample rate
-    # duration = 5  # Duration in seconds
-    # myrecording = sd.rec(
-    #     int(duration * fs), samplerate=fs, channels=2, blocking=True
-    #     )
-    # config = get_config()
-    # config = {'microphones': {None: {'samplerate'}}}
-    # sf.write('output/output.wav', myrecording, fs)
-    # sd.play(myrecording, fs)
-    # sd.wait()
